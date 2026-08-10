7b33bd84474ca0c2cb05da36e07a8ed7eb95a352
PawanKrSah <pawankrsah3@gmail.com>
2026-08-10 10:15:37 +0530

    added modules/users.js

---
+++ server/models/Users.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    name: {type: String},
+    email: {type: String},
+    otp: {type: String},
+
+});
+
+mongoose.model("users", userSchema);
